0d02852ebb2e058d2e5bd402b30e794565a2edc6
Jesse Mitchell <jallmitch@tamu.edu>
2017-03-16 18:19:22 -0500

    Add Free and Paid flavors
    Stubbing out flavors.

---
+++ MyApplication/app/src/paid/java/com/example/jessemitchell/builditbigger/paid/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.jessemitchell.builditbigger.paid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.jessemitchell.builditbigger.EndpointAsyncTask;
+import com.example.jessemitchell.builditbigger.R;
+import com.example.jokeactivitylib.JokeActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_paid);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+
+        try {
+            String joke = new EndpointAsyncTask().execute("Manfred").get();
+            // use AsyncTask to pull joke
+            Intent jalIntent = new Intent(this, JokeActivity.class);
+            jalIntent.putExtra("Joke", joke);
+            startActivity(jalIntent);
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+
+}
